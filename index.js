89938d7559266048ef02b6f7c23fb71f957a3070
Paul Rey <paul@iterop.com>
2021-09-14 23:31:31 +0200

    WIP

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const preserveInstance = (socket, instance) => {
 
 const workspaces = io.of(/^\/instance-.*$/);
 workspaces.on("connection", (socket) => {
-    console.log(socket)
-    const namespace = socket.nsp;
+    const namespace = socket.name;
     const instance = namespace.replace(/^\/instance-/, "");
     if (connections[instance] === undefined) {
         connections[instance] = {
