3af23a1d4b36b447df526afc0decc72a9d1cd5b9
Paul Rey <paul@iterop.com>
2021-09-14 22:49:56 +0200

    WIP

---
+++ index.js
@@ -0,0 +1,45 @@
+const app = require("express")();
+const httpServer = require("http").createServer(app);
+const options = { /* ... */ };
+const io = require("socket.io")(httpServer, options);
+
+const connections = {};
+
+const preserveInstance = (socket, instance) => {
+    setInterval(() => {
+        socket.emit("preserve-instance", instance);
+    }, 10000);
+}
+
+io.of(/^\/instance-.*$/).on("connection", (socket) => {
+    const namespace = socket.nsp;
+    const instance = namespace.replace(/^\/instance-/, "");
+    if (connections[instance] === undefined) {
+        connections[instance] = {
+            "items": [],
+            "sockets": []
+        };
+    }
+    console.log(`Socket ${socket.id} connected (${instance}`);
+    const items = connections[instance].items;
+    const sockets = connections[instance].sockets;
+
+    sockets.push(socket);
+
+    preserveInstance(socket, instance);
+
+    socket.on('item-found', item => {
+        if (items.indexOf(item) === -1) {
+            items.push(item);
+            if (item === "carte_grise") {
+                socket.emit('allow-monitoring');
+            }
+        }
+    });
+
+    socket.on("disconnect", () => {
+        sockets.splice(sockets.indexOf(socket), 1)
+    });
+});
+
+httpServer.listen(3000);
